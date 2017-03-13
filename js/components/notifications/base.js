a268b52145fa13e33bd5ef56d4070c62eb458402
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-03-13 11:48:30 +0100

    Remove admin entrypoint in webpack and related files

--- js/components/notifications/base.js
+++
@@ -1,6 +0,0 @@
-export default {
-    replace: true,
-    props: {
-        details: Object
-    }
-};
--- js/models/resource.js
+++
@@ -1,5 +0,0 @@
-import {Model} from 'models/base';
-import log from 'logger';
-
-
-export default class Resource extends Model {};
